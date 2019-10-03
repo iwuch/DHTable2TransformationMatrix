aa3e7ab219ed67a8a980cdbb237523dd901d1181
iwuch <wgl888750504@163.com>
2019-10-03 23:09:36 +0800

    introduce the Twb matrix as the final output
    Define the output of the function more clearly. Input the DHTable, and we will get the Twb matrix as the output, the other parameters could be calculated by the matrix,

--- DHTable2HTM.m
+++ DHTable2HTM.m
@@ -1,39 +1,49 @@
 %% Input the DHTable, Output the formular for each parameter in the
 %% Transformation matrix
 
-syms theta1 theta2 theta3 theta4 theta5 theta6 a2 a3 d3 d4
+syms theta1 theta2 theta3 theta4 theta5 theta6 a2 a3 d2 d3 d4 L1 L2
+
+% DHTable = [0        0       0       theta1;
+%            -pi/2    0       0       theta2;
+%            0        a2      d3      theta3;
+%            -pi/2    a3      d4      theta4;
+%            pi/2     0       0       theta5;
+%            -pi/2    0       0       theta6]
+       
+% DHTable = [0        0       0       theta1;
+%            0        L1      0       theta2;
+%            0        L2      0       theta3]
 
 DHTable = [0        0       0       theta1;
-           -pi/2    0       0       theta2;
-           0        a2      d3      theta3;
-           -pi/2    a3      d4      theta4;
-           pi/2     0       0       theta5;
-           -pi/2    0       0       theta6]
-       
+           pi/2     0       d2      0]
+
 alpha   = DHTable(:,1)
 a       = DHTable(:,2)
 d       = DHTable(:,3)
 theta   = DHTable(:,4)
 DHsize  = size(DHTable)
 dof     = DHsize(1)
-
+Twb     = eye(4,4)
 for i = 1:dof
-    T{i} = simplify(DHTable2T(theta(i), d(i), a(i), alpha(i))); % The function is Question 3(a)
+    T{i} = simplify(DHTable2T(theta(i), d(i), a(i), alpha(i))); 
+    Twb  = Twb*T{i}
 end
 
-T06 = T{1}*T{2}*T{3}*T{4}*T{5}*T{6}
+% T02 = T{1}*T{2}
+% T03 = T{1}*T{2}*T{3}
+% T06 = T{1}*T{2}*T{3}*T{4}*T{5}*T{6}
 
-r11 = T06(1,1)
-r21 = T06(2,1)
-r31 = T06(3,1)
-r12 = T06(1,2)
-r22 = T06(2,2)
-r32 = T06(3,2)
-r13 = T06(1,3)
-r23 = T06(2,3)
-r33 = T06(3,3)
-px  = T06(1,4)
-py  = T06(2,4)
-pz  = T06(3,4)
-
-subs(r11,{theta1 theta2 theta3 theta4 theta5 theta6 a2 a3 d3 d4},{pi/3 pi/3 pi/3 pi/3 pi/3 pi/3 1 2 3 4})
+% r11 = T06(1,1)
+% r21 = T06(2,1)
+% r31 = T06(3,1)
+% r12 = T06(1,2)
+% r22 = T06(2,2)
+% r32 = T06(3,2)
+% r13 = T06(1,3)
+% r23 = T06(2,3)
+% r33 = T06(3,3)
+% px  = T06(1,4)
+% py  = T06(2,4)
+% pz  = T06(3,4)
+% 
+% subs(r11,{theta1 theta2 theta3 theta4 theta5 theta6 a2 a3 d3 d4},{pi/3 pi/3 pi/3 pi/3 pi/3 pi/3 1 2 3 4})
